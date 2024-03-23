69d4bbc854f7c3ab230f7fdffaf921a4606c881c
LoganKuss <kusslogan@gmail.com>
2024-03-23 00:06:34 -0500

    Prints report and working as it should be

--- main.py
+++ main.py
@@ -3,13 +3,32 @@ def main():
     text = get_book_text(path_of_book)
     num_words = num_of_words(text)
     num_of_chars = num_of_characters(text)
-    print(f"The number of words in Mary Shelley's Frankenstein are {num_words}")
-    print(num_of_chars)
+    char_sorted_list = chars_dict_to_sorted_list(num_of_chars)
+    
+    print(f"---- Begin report of {path_of_book}-----")
+    print(f"There are {num_words} found in the book/n")
+
+    for item in char_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("---End Report---")
 
 def num_of_words(text):
     words = text.split()
     word_num = len(words)
     return word_num
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for char in num_chars_dict:
+        sorted_list.append({"char": char, "num": num_chars_dict[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 def get_book_text(path):
     with open(path) as f:
