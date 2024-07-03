a224bbceee7278498ec900957e36b6ad978efe72
LoganKuss <kusslogan@gmail.com>
2024-07-02 21:55:36 -0500

    Added the ability to choose file to analyze

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
+import os
+
 def main():
-    path_of_book = "books/frankenstein.txt"
+    original_path = "books/"
+    files = [f for f in os.listdir(original_path) if os.path.isfile(os.path.join(original_path, f))]
+    if not files:
+        print("No books found in the 'books' directory")
+        return
+    
+    for i, file in enumerate(files, start=1):
+        print(f"{i}. {files}: ")
+
+    try:
+        book_choice = int(input("Type 1, 2, 3, etc."))
+        if not book_choice >= 1 and book_choice <= len(files):
+            raise ValueError("Choice out of range")
+    except ValueError as e:
+        print(f"Invalid input: {e}")
+
+    name_of_book = files[book_choice - 1]
+    path_of_book = os.path.join(original_path, name_of_book)
     text = get_book_text(path_of_book)
     num_words = num_of_words(text)
     num_of_chars = num_of_characters(text)
     char_sorted_list = chars_dict_to_sorted_list(num_of_chars)
     
-    print(f"---- Begin report of {path_of_book}-----")
+    print(f"---- Begin report of {path_of_book}----")
     print(f"There are {num_words} found in the book/n")
 
     for item in char_sorted_list:
